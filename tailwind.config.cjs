46022dbf1ea9f9e087c7e756d3a99b50c6fda119
Gamaunov <gamaunov1911@gmail.com>
2023-03-21 17:30:51 +0300

    navbar menu + enum

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "gray-20": "#f8f4eb",
-        "gray-50": "#efe6e6",
-        "gray-100": "#dfcccc",
-        "gray-500": "#5e0000",
-        "primary-100": "#ffe1e0",
-        "primary-300": "#ffa6a3",
-        "primary-500": "#ff6b66",
-        "secondary-400": "#ffcd58",
-        "secondary-500": "#ffc132",
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowred": "linear-gradient(90deg, #ff616a 0%, #ffc837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')"
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
@@ -27,13 +28,13 @@ module.exports = {
         abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
-      }
+      },
     },
     screens: {
       xs: "480px",
       sm: "768px",
       md: "1060px",
-    }
+    },
   },
   plugins: [],
-}
+};
